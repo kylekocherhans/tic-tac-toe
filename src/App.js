d74b3fb9a5f13c9c36f17529acf983f3781d972b
Kyle Kocherhans <kylekocherhans@gmail.com>
2023-02-17 13:58:08 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Square from "./Square";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [squares, setSquares] = useState(["","","","","","","","",""]);
+    const [player, setPlayer] = useState(true);
+
+    const calculateWinner = (arr) => {
+        let message = "Who will win?";
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        // for (let i = 0; i < lines.length; i++) {
+        for (const line of lines) {
+            const [a, b, c] = line;
+
+            if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+                return `${arr[a]} won!`
+            }
+        }
+
+        return "Who will win?";
+    };
+
+    const handleClick = () => {
+        setSquares(["", "", "", "", "", "", "", "", ""]);
+        setPlayer(true);
+    };
+
+    return (
+        <div className="App">
+            <span>{calculateWinner(squares)}</span>
+            <div className="container">
+                {squares.map((val, index) => {
+                    return (
+                        <Square
+                            squares={squares}
+                            setSquares={setSquares}
+                            player={player}
+                            setPlayer={setPlayer}
+                            squareValue={val}
+                            index={index}
+                            key={index}
+                        />
+                    );
+                })}
+            </div>
+            <button onClick={handleClick}>Reset</button>
+        </div>
+    );
 }
 
 export default App;
